6f2f6ea63b451fefb0ea4418ca5c9d4d97822626
nicoly <nicoly.email@email.com>
2023-05-24 11:27:57 -0300

    home and login

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ const Database = require('sqlite-async')
 Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute() {
-    console.log("db")
+    console.log("db") 
 }
 
 // https://www.youtube.com/watch?v=yEpiT-N2DUc
